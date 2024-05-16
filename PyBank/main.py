9bfd0947d9faa2dbe9619c945cf792ca5662f625
emma1698 <epr1698@gmail.com>
2024-05-16 09:25:37 -0400

    update csvreader

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,14 @@
+import os
 import csv
 
 csv_path= "Resources/budget_data.csv"
 
-with open(csv_path, mode="r")as csv_file:
-    reader = csv.reader(csv_file)
-    header=next(reader)
-    for row in reader:
+with open(csv_path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter= ',')
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
         print(row)
-                
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,14 @@
+import os
 import csv
 
 csv_path= "Resources\election_data.csv"
 
-with open(csv_path, mode="r")as csv_file:
-    reader = csv.reader(csv_file)
-    header=next(reader)
-    for low in reader:
-        print(low)
-        
+with open(csv_path)as csvfile:
+    csvreader = csv.reader(csvfile, delimiter= ',')
     
+    csv_header=next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+    for line in csvreader:
+        print(line)
+        
