97880475e97f5f474724b092a713415d4c87b418
emma1698 <epr1698@gmail.com>
2024-05-16 19:01:34 -0400

    midway done

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,37 +1,39 @@
+# Creating the file/cvs file
 import os
 import csv
 
+# All Variable
+    total_months = 0
+    total_profit_losses = 0
+    changes_profit_losses = []
+    profit_losses= []
+    months=[]
+
+# Set path for file
 csv_path =os.path.join("Resources" , "budget_data.csv")
 
+# Opening cvs file
 with open(csv_path)as csv_file:
     csvreader= csv.reader (csv_file, delimiter= ',')
     csvheader=next(csvreader)
     print('Financial Analysis')
     print("__________________")
-    
 
-# The total number of months included in the dataset
-    total_months = []
-# The net total amount of "Profit/Losses" over the entire period
-    total_profit_losses = []
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-    changes_profit_losses = []
- # The greatest increase in profits (date and amount) over the entire period
-    greatest_increase_profit= 0
-# The greatest decrease in profits (date and amount) over the entire period   
-    greatest_decrease_profit= 0
-
+# Looping through the data store and calculating total amount
     for row in csvreader:
         total_months += 1
         total_profit_losses += int(row[1])
-        total_months.append(row[0])
-        total_profit_losses.append
-        changes_profit_losses.append
+        months.append(row[0])
+        profit_losses.append(int(row[1]))
+        changes_profit_losses.append(profit_losses[total_months -1]-profit_losses[total_months -2])
 
+ # Getting the greatest increase and greatest decreased from change
+    total_changes =sum(changes_profit_losses)
+    average_changes =round(total_changes/(len(changes_profit_losses) -1), 2)
+    increase_profit = max()
 
 
     # print(current_profit)
-    print(total_months)
-    print(total_profit_losses)
+    
     
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,29 @@
+# Creating the file/cvs file
 import os
 import csv
 
+# Creating path for file
 csvpath= os.path.join('Resources','election_data.csv')
-        
+
+# Opening the csv file
 with open(csvpath)as csvfile:
-    csvreader= csv.reader(csvfile, delimiter=',')
+    csvreader= csv.reader(csvfile, delimiter=',') 
     csv_header= next(csvreader)
+
+# Listing variables
+candidates= []
+number_votes= []
+votes_receive= []
+total = 0
+Raymond= 0
+Charles=0
+Diana=0
+
+# Counting the total number of votes
+for row in csvreader:
+    total += 1
+    
+    
+
     
   
