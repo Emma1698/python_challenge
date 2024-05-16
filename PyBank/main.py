c3fea799b2330f0a651f1008e1298391e9c3f37b
emma1698 <epr1698@gmail.com>
2024-05-16 18:59:12 -0400

    halfway done

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,37 @@
 import os
 import csv
 
-csv_path= "Resources/budget_data.csv"
+csv_path =os.path.join("Resources" , "budget_data.csv")
 
-with open(csv_path) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= ',')
-    print(csvreader)
+with open(csv_path)as csv_file:
+    csvreader= csv.reader (csv_file, delimiter= ',')
+    csvheader=next(csvreader)
+    print('Financial Analysis')
+    print("__________________")
+    
 
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+# The total number of months included in the dataset
+    total_months = []
+# The net total amount of "Profit/Losses" over the entire period
+    total_profit_losses = []
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    changes_profit_losses = []
+ # The greatest increase in profits (date and amount) over the entire period
+    greatest_increase_profit= 0
+# The greatest decrease in profits (date and amount) over the entire period   
+    greatest_decrease_profit= 0
 
     for row in csvreader:
-        print(row)
+        total_months += 1
+        total_profit_losses += int(row[1])
+        total_months.append(row[0])
+        total_profit_losses.append
+        changes_profit_losses.append
+
+
+
+    # print(current_profit)
+    print(total_months)
+    print(total_profit_losses)
+    
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,10 @@
 import os
 import csv
 
-csv_path= "Resources\election_data.csv"
-
-with open(csv_path)as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= ',')
-    
-    csv_header=next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    
-    for line in csvreader:
-        print(line)
+csvpath= os.path.join('Resources','election_data.csv')
         
+with open(csvpath)as csvfile:
+    csvreader= csv.reader(csvfile, delimiter=',')
+    csv_header= next(csvreader)
+    
+  
